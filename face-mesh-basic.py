21921c22f16d141b1c21e62f9ecc8627fefdc24f
prateek-ml <prateekb1912@gmail.com>
2021-07-13 09:09:47 +0530

    Process the RGB image into the face mesh object

--- face-mesh-basic.py
+++ face-mesh-basic.py
@@ -6,8 +6,13 @@ cap = cv2.VideoCapture(0)
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh()
 
+mpDraw = mp.solutions.drawing_utils
+
 while True:
     _, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = faceMesh.process(imgRGB)
 
     cv2.flip(img, 1)
     cv2.imshow("Face Mesh", img)
